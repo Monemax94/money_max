3cf7ba218931ebd7d9c578e8c7d0d748f8641f55
Monemax94 <mondayodoh94gmail.com>
2023-05-29 12:27:22 +0100

    Async email validation implemented at the backend and front end

--- expenseswebsite/expenseswebsite/static/js/register.js
+++ expenseswebsite/expenseswebsite/static/js/register.js
@@ -1,9 +1,44 @@
-const usernameField=document.querySelector('#usernameField');
-const feedBackArea=document.querySelector('.invalid_feedback');
+const usernameField=document.querySelector("#usernameField");
+const feedBackArea=document.querySelector(".invalid_feedback");
+const emailField=document.querySelector("#emailField");
+const emailFeedBackArea=document.querySelector(".emailFeedBackArea");
+const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
+
+emailField.addEventListener("keyup", (e) => {
+    const emailVal = e.target.value;
+    
+
+    emailField.classList.remove("is-invalid");
+    emailFeedBackArea.style.display="none"
+
+    
+    if (emailVal.length > 0) {
+      fetch("/authentication/validate-email", {
+            body: JSON.stringify({ email: emailVal }),
+            method: "POST",
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("data", data);
+                if (data.email_error){
+                    emailField.classList.add("is-invalid");
+                    emailFeedBackArea.style.display = "block";
+                    emailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`
+                }
+            });
+    }
+
+})
+
+
+
 
 usernameField.addEventListener("keyup", (e) => {
-    console.log('77777', 7777);
     const usernameVal = e.target.value;
+    
+    usernameSuccessOutput.style.display = "block";
+
+    usernameSuccessOutput.textContent=`checking ${usernameVal}`
 
     usernameField.classList.remove("is-invalid");
     feedBackArea.style.display="none"
@@ -17,6 +52,7 @@ usernameField.addEventListener("keyup", (e) => {
             .then((res) => res.json())
             .then((data) => {
                 console.log("data", data);
+                usernameSuccessOutput.style.display = "none";
                 if (data.username_error){
                     usernameField.classList.add("is-invalid");
                     feedBackArea.style.display="block"
