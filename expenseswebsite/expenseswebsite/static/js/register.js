baa86322d0bbe47aacd3b3a101980ab2e3bde6b2
Monemax94 <mondayodoh94gmail.com>
2023-05-29 16:32:44 +0100

    Implementing toggle password input

--- expenseswebsite/expenseswebsite/static/js/register.js
+++ expenseswebsite/expenseswebsite/static/js/register.js
@@ -2,7 +2,22 @@ const usernameField=document.querySelector("#usernameField");
 const feedBackArea=document.querySelector(".invalid_feedback");
 const emailField=document.querySelector("#emailField");
 const emailFeedBackArea=document.querySelector(".emailFeedBackArea");
+const passwordField=document.querySelector("#passwordField");
 const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
+const showPasswordToggle=document.querySelector(".showPasswordToggle");
+
+const handleToggleInput = (e) => {
+    if (showPasswordToggle.textContent === "SHOW") {
+        showPasswordToggle.textContent = "HIDE";
+        passwordField.setAttribute("type", "text");
+
+    } else {
+        showPasswordToggle.textContent = "SHOW";
+        passwordField.setAttribute("type", "password");
+    }
+};
+
+showPasswordToggle.addEventListener("click", handleToggleInput);
 
 emailField.addEventListener("keyup", (e) => {
     const emailVal = e.target.value;
