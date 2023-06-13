c27e89446d6ee1ffa0f2be61ee1bc8bec404944e
Monemax94 <mondayodoh94gmail.com>
2023-06-13 18:15:51 +0100

    Implementing user currency prefrence settins, add expenses, edit expenses and search expenses CRUD operation and AJax search

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-
+from django.views.decorators.csrf import csrf_exempt
 
 
 
 
 urlpatterns = [
     path('', views.index, name="expenses"),
-    path('add_expense', views.add_expense, name="add_expense")
+    path('add_expense', views.add_expense, name="add_expense"),
+    path('edit-expense/<int:id>', views.expense_edit, name="expense-edit"),
+    path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
+    path('search-expenses', csrf_exempt(views.search_expenses), name="search_expenses")
 ]
